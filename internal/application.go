362896b8905763a7be07094600be999b93196fce
ilya romanov <romanov.i.u@yandex.ru>
2023-03-25 19:16:22 +0400

    Add parse command into help

--- internal/application.go
+++ internal/application.go
@@ -97,6 +97,7 @@ func (a *Application) fieldTypesDefinitions() error {
 
 func (a *Application) help() error {
 	fmt.Printf("%s - %s\n", color.ColoredString(color.Green, seedCommand), color.ColoredString(color.Yellow, "Fill database generated data"))
+	fmt.Printf("%s - %s\n", color.ColoredString(color.Green, parseCommand), color.ColoredString(color.Yellow, "Get and write tables for databases."))
 	fmt.Printf("%s - %s\n", color.ColoredString(color.Green, fieldTypeDefinitions), color.ColoredString(color.Yellow, "Show all allowed fields"))
 	fmt.Printf("%s - %s\n", color.ColoredString(color.Green, schemaDependencies), color.ColoredString(color.Yellow, "Show all dependecies btw tables and databases in schema"))
 	fmt.Printf("%s - %s\n", color.ColoredString(color.Green, modifiersList), color.ColoredString(color.Yellow, "Show all allowed modifiers"))
