27b13151062e0bc21d327a29f875926c75cb9b77
ilya romanov <romanov.i.u@yandex.ru>
2023-04-15 10:08:27 +0400

    add tests for plugins store

---
+++ internal/modifiers/plugins_test.go
@@ -0,0 +1,63 @@
+package modifiers
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type mOne struct {
+	code string
+	fn   ModifierFn
+}
+
+func (m *mOne) GetCode() string {
+	return m.code
+}
+
+func (m *mOne) GetDescription() string {
+	return ""
+}
+
+func (m *mOne) GetFn() ModifierFn {
+	return m.fn
+}
+
+func TestModifierStore_Apply(t *testing.T) {
+	mStore := &ModifierStore{
+		store: map[string]Modifier{
+			"one": &mOne{
+				code: "one",
+				fn: func(v any) (any, error) {
+					return "one " + v.(string), nil
+				},
+			},
+		},
+	}
+
+	expected := "one hello"
+	actual, _ := mStore.Apply("one", "hello")
+	assert.Equal(t, expected, actual)
+}
+
+func TestModifierStore_ApplyList(t *testing.T) {
+	mStore := &ModifierStore{
+		store: map[string]Modifier{
+			"one": &mOne{
+				code: "one",
+				fn: func(v any) (any, error) {
+					return "one " + v.(string), nil
+				},
+			},
+			"two": &mOne{
+				code: "two",
+				fn: func(v any) (any, error) {
+					return "two " + v.(string), nil
+				},
+			},
+		},
+	}
+
+	expected := "two one hello"
+	actual, _ := mStore.ApplyList([]string{"one", "two"}, "hello")
+	assert.Equal(t, expected, actual)
+}
