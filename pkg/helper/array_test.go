bd7693938fd2d65d47598521ae8282bda91fada5
ilya romanov <romanov.i.u@yandex.ru>
2023-04-15 09:58:55 +0400

    add tests for pkg array

--- pkg/helper/array_test.go
+++ pkg/helper/array_test.go
@@ -1,1 +1,13 @@
 package helper
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestInArray(t *testing.T) {
+	expectedFind := []string{"one", "two", "three", "four"}
+
+	assert.True(t, InArray[string](expectedFind, "one"))
+	assert.False(t, InArray[string](expectedFind, "five"))
+}
